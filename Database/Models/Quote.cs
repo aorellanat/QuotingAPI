784744c69209890e0fb91274847b50acbd657c82
Adriana <adrianaorellana34@gmail.com>
2019-11-27 09:32:09 -0400

    Quote model added to Database and Controllers

---
+++ Database/Models/Quote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    class Quote
+    {
+        public int Id { get; set; }
+        public string IdClient { get; set; }
+        public string IdProducts { get; set; }
+        public float Price { get; set; }
+        public DateTime Date { get; set; }
+        public bool Sold { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ QuotingAPI/Models/QuoteDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotingAPI.Models
+{
+    public class QuoteDTO
+    {
+        public int Id { get; set; }
+        public string IdClient { get; set; }
+        public string IdProducts { get; set; }
+        public float Price { get; set; }
+        public DateTime Date { get; set; }
+        public bool Sold { get; set; }
+        public int Quantity { get; set; }
+    }
+}
